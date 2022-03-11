2c8552c039340a4a18e188732d57ce9e63651c5d
nihalshahria <nihalshahria@gmail.com>
2022-03-11 16:09:05 +0600

    error handling updated

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
+const Doctor = require("../models/doctor");
 const Blog = require("../models/blogModel");
 const Comment = require("../models/commentModel");
-const Patient = require("../models/patient");
-const Doctor = require("../models/doctor");
 const HttpError = require('../models/http-error');
+const catchAsync = require("../utils/catchAsync");
 
 
-
-exports.createBlog = async (req, res, next) => {
-    console.log(req.body);
+exports.createBlog = catchAsync(async (req, res, next) => {
     const { blogTitle, blogBody, blogCategory } = req.body;
-
     const userId = req.userData.id;
     const user = await Doctor.findById(userId);
-
-    console.log(user.id, req.userData.type, user.name);
-    let imgPath;
-    if (req.files.length > 0) {
-        imgPath = req.files[0].path;
-    }
+    const imgPath = req.files.length > 0 ? req.files[0].path : "";
+    const doctor = await Doctor.findById(userId);
+    if (!doctor) throw new HttpError("User Not Found", 404);
     const newBlog = new Blog({
         blogTitle: blogTitle,
         blogBody: blogBody,
@@ -28,92 +22,51 @@ exports.createBlog = async (req, res, next) => {
         photo: imgPath,
         upvotes: []
     });
-    console.log();
-    let doctor;
-    try {
-        doctor = await Doctor.findById(userId);
-    } catch (err) {
-        const error = new HttpError("Failed to publish Blog , please try again", 500);
-        return next(error);
-    }
-    if (!doctor) {
-        const error = new HttpError("User Not Found", 404);
-        return next(error);
-    }
-
-    try {
-        const session = await mongoose.startSession();
-        session.startTransaction();
-        await newBlog.save({ session: session });
-        doctor.blogs.push(newBlog);
-        await doctor.save({ session: session });
-        await session.commitTransaction();
-    } catch (err) {
-        console.log(err.message);
-        const error = new HttpError("Failed to publish Blog, please try again", 500);
-        return next(error);
-    }
-
-
-
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newBlog.save({ session: session });
+    doctor.blogs.push(newBlog);
+    await doctor.save({ session: session });
+    await session.commitTransaction();
     res.status(201).json({
         message: "successful",
         data: {
             newBlog,
         },
     });
-};
+});
 
-exports.getAllBlogs = async (req, res, next) => {
-    let { category, sortBy } = req.query;
-
-    let blogs;
+exports.getAllBlogs = catchAsync(async (req, res, next) => {
+    const { category, sortBy } = req.query;
     let filteredBlogs;
-    try {
-        blogs = await Blog.find().populate("commentId");
-        if (!category) {
-            filteredBlogs = blogs;
-        } else {
-
-            filteredBlogs = blogs.filter(blog => {
-
-                console.log(blog.blogCategory, category)
-                return blog.blogCategory === category
-            })
-            // blogs = await Blog.find({ blogCategory: category }).populate(); // blog search by category not working
-        }
-    } catch (error) {
-        console.log(error.message)
-        return next(new HttpError("Something Went Wrong", 401));
+    const blogs = await Blog.find().populate("commentId");
+    if (!category) {
+        filteredBlogs = blogs;
+    } else {
+        filteredBlogs = blogs.filter(blog => {
+            return blog.blogCategory === category
+        });
     }
-    if(!sortBy){
-        // sortBy = "dateAsc";
-        sortBy = "dateDesc";
-    }
-    // 1645251244065
-    // 1645251220511
+    if (!sortBy) sortBy = "dateDesc";
     let filteredBlogsSorted;
     if (sortBy === "upvotesAsc") {
-        filteredBlogsSorted=filteredBlogs.sort((a, b) => {
+        filteredBlogsSorted = filteredBlogs.sort((a, b) => {
             return a.upvotes.length - b.upvotes.length
         })
-    } else if(sortBy === "upvotesDesc") {
-        filteredBlogsSorted= filteredBlogs.sort((a, b) => {
+    } else if (sortBy === "upvotesDesc") {
+        filteredBlogsSorted = filteredBlogs.sort((a, b) => {
             return b.upvotes.length - a.upvotes.length
         })
-    }else if(sortBy ===`dateAsc`){ 
+    } else if (sortBy === `dateAsc`) {
         filteredBlogsSorted = filteredBlogs.sort((a, b) => {
             return Number(a.createdAt) - Number(b.createdAt)
         })
-    }else if(sortBy ===`dateDesc`){
+    } else if (sortBy === `dateDesc`) {
         filteredBlogsSorted = filteredBlogs.sort((a, b) => {
-            return  Number(b.createdAt) - Number(a.createdAt)
+            return Number(b.createdAt) - Number(a.createdAt)
         })
     }
     filteredBlogs = filteredBlogsSorted;
-    // filteredBlogs.sort(function (blog1, blog2) {
-    //     return x.timestamp - y.timestamp;
-    // })
     res.status(200).json({
         message: "successful",
         No_of_blogs: blogs.length,
@@ -121,64 +74,40 @@ exports.getAllBlogs = async (req, res, next) => {
             filteredBlogs,
         },
     });
-};
+});
 
-exports.getBlogsOfUser = async (req, res, next) => {
-    let user;
-    let id = req.params.id;
-
-    try {
-        if (req.userData.type === "doctor") {
-            user = await Doctor.findById(id).populate('blogs');
-        }
-    } catch (error) {
-        console.log(error.message);
+exports.getBlogsOfUser = catchAsync(async (req, res, next) => {
+    const id = req.params.id;
+    if (req.userData.type !== "doctor") {
+        throw new HttpError("Unauthorized", 401);
     }
-
-    console.log(user)
+    const user = await Doctor.findById(id).populate('blogs');
     res.status(200).json({
         message: "successful",
         data: {
             blogs: user.blogs.map(id => id.toObject({ getters: true })),
         },
     });
-};
+});
 
-exports.getBlog = async (req, res, next) => {
+exports.getBlog = catchAsync(async (req, res, next) => {
     const id = req.params.id;
-    let blog;
-    try {
-        blog = await Blog.findById(id).populate("commentId");
-    } catch (error) {
-        return next(new HttpError("Something Went Wrong", 401));
-    }
-    if (!blog) {
-        return next(new HttpError("Blog Not Found", 404));
-    }
-
+    const blog = await Blog.findById(id).populate("commentId");
+    if (!blog) throw new HttpError("Blog Not Found", 404);
     res.status(200).json({
         message: "successful",
         data: {
             blog,
         },
     });
-};
+});
 
-exports.commentBlog = async (req, res, next) => {
+exports.commentBlog = catchAsync(async (req, res, next) => {
     const blogId = req.params.id;
     const { comment } = req.body;
-    if (req.userData.type !== "patient") {
-        return next(new AppError("Unauthorized", 400));
-    }
-    let blog
-    try {
-        blog = await Blog.findById(blogId);
-    } catch (error) {
-        return next(new HttpError("Something Went Wrong", 401));
-    }
-    if (!blog) {
-        return next(new HttpError("Blog Not Found", 404));
-    }
+    if (req.userData.type !== "patient") throw new AppError("Unauthorized", 400);
+    const blog = await Blog.findById(blogId);
+    if (!blog) throw new HttpError("Blog Not Found", 404);
     const newComment = new Comment({
         blogId: blogId,
         comment: comment,
@@ -186,67 +115,48 @@ exports.commentBlog = async (req, res, next) => {
         name: req.userData.name,
         upvotes: []
     });
-    try {
-        const session = await mongoose.startSession();
-        session.startTransaction();
-        await newComment.save({ session: session });
-        blog.commentId.push(newComment);
-        await blog.save({ session: session });
-        await session.commitTransaction();
-    } catch (err) {
-        console.log(err.message);
-        const error = new HttpError("Failed to publish Comment, please try again", 500);
-        return next(error);
-    }
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newComment.save({ session: session });
+    blog.commentId.push(newComment);
+    await blog.save({ session: session });
+    await session.commitTransaction();
     res.status(201).json({
         message: "successful",
         data: {
             newComment,
         },
     });
-};
+});
 
-exports.getComments = async (req, res, next) => {
-    let blog;
-    let id = req.params.id;
-    try {
-        blog = await Blog.findById(id).populate('commentId');
-    } catch (error) {
-        console.log(error.message);
-        return next(new HttpError("Something Went Wrong", 401));
-    }
+exports.getComments = catchAsync(async (req, res, next) => {
+    const id = req.params.id;
+    const blog = await Blog.findById(id).populate('commentId');
     res.status(200).json({
         message: "successful",
         data: {
             comments: blog.commentId.map(id => id.toObject({ getters: true })),
         },
     });
-};
+});
 
-
-
-exports.upvoteBlog = async (req, res, next) => {
+exports.upvoteBlog = catchAsync(async (req, res, next) => {
     const blogId = req.params.id;
     const id = req.userData.id;
     const blog = await Blog.findById(blogId);
     const isUpvoted = blog.upvotes && blog.upvotes.includes(id);
-
-    let option;
-    if (!isUpvoted) {
-        option = "$addToSet";
-    } else option = "$pull";
-    let updatedBlog = await Blog.findByIdAndUpdate(
+    const option = isUpvoted ? "$pull" : "$addToSet";
+    const updatedBlog = await Blog.findByIdAndUpdate(
         blogId,
         {
             [option]: { upvotes: id },
         },
         { new: true }
     );
-
     res.status(201).json({
         message: "success",
         data: {
             blog: updatedBlog,
         },
     });
-};
+});
--- backend/controllers/doctor-controllers.js
+++ backend/controllers/doctor-controllers.js
@@ -10,286 +10,178 @@ const Appointment = require("../models/appointment");
 const { validationResult } = require("express-validator");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  if (!validationResult(req).isEmpty())
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
-  const {
-    name,
-    gender,
-    phoneNo,
-    email,
-    password,
-    medicalId,
-    specializations,
-    qualifications,
-    workplaces,
-  } = req.body;
+    if (!validationResult(req).isEmpty())
+        throw new HttpError("Invalid inputs passed, please check your data", 422);
+    const {
+        name,
+        gender,
+        phoneNo,
+        email,
+        password,
+        medicalId,
+        specializations,
+        qualifications,
+        workplaces,
+    } = req.body;
 
-  const existingDoctor = await Doctor.findOne({ email: email });
+    const existingDoctor = await Doctor.findOne({ email: email });
 
-  if (existingDoctor)
-    throw new HttpError("User already exists, please login", 422);
+    if (existingDoctor)
+        throw new HttpError("User already exists, please login", 422);
 
-  const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-  const createdDoctor = await Doctor.create({
-    name,
-    gender,
-    phoneNo,
-    email,
-    password: hashedPassword,
-    profileImage: req.files[0].path,
-    medicalId,
-    licenseFront: req.files[1].path,
-    licenseBack: req.files[2].path,
-    specializations: JSON.parse(specializations),
-    qualifications: JSON.parse(qualifications),
-    workplaces: JSON.parse(workplaces),
-    appointments: [],
-    blogs: [],
-  });
-  const token = jwt.sign(
-    {
-      id: createdDoctor.id,
-      email: createdDoctor.email,
-      type: "doctor",
-      name: createdDoctor.name,
-    },
-    process.env.JWT_KEY,
-    {
-      expiresIn: "6d",
-    }
-  );
-  const user = _.pick(createdDoctor, ["id", "name", "email"]);
-  res.status(201).json({ ...user, token });
+    const createdDoctor = await Doctor.create({
+        name,
+        gender,
+        phoneNo,
+        email,
+        password: hashedPassword,
+        profileImage: req.files[0].path,
+        medicalId,
+        licenseFront: req.files[1].path,
+        licenseBack: req.files[2].path,
+        specializations: JSON.parse(specializations),
+        qualifications: JSON.parse(qualifications),
+        workplaces: JSON.parse(workplaces),
+        appointments: [],
+        blogs: [],
+    });
+    const token = jwt.sign(
+        {
+            id: createdDoctor.id,
+            email: createdDoctor.email,
+            type: "doctor",
+            name: createdDoctor.name,
+        },
+        process.env.JWT_KEY,
+        {
+            expiresIn: "6d",
+        }
+    );
+    const user = _.pick(createdDoctor, ["id", "name", "email"]);
+    res.status(201).json({ ...user, token });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-  if (!validationResult(req).isEmpty())
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
-  const { email, password } = req.body;
-  const existingDoctor = await Doctor.findOne({ email: email });
-  if (!existingDoctor)
-    throw new HttpError("Invalid credentials, could not log you in.", 401);
-  const isValidPassword = await bcrypt.compare(
-    password,
-    existingDoctor.password
-  );
-  if (!isValidPassword)
-    throw new HttpError("Invalid credentials, could not log you in.", 401);
-  const token = jwt.sign(
-    {
-      id: existingDoctor.id,
-      email: existingDoctor.email,
-      type: "doctor",
-      name: existingDoctor.name,
-    },
-    process.env.JWT_KEY,
-    {
-      expiresIn: "6d",
-    }
-  );
-  const user = _.pick(existingDoctor, ["id", "name", "email"]);
-  res.status(201).json({ ...user, token });
+    if (!validationResult(req).isEmpty())
+        throw new HttpError("Invalid inputs passed, please check your data", 422);
+    const { email, password } = req.body;
+    const existingDoctor = await Doctor.findOne({ email: email });
+    if (!existingDoctor)
+        throw new HttpError("Invalid credentials, could not log you in.", 401);
+    const isValidPassword = await bcrypt.compare(
+        password,
+        existingDoctor.password
+    );
+    if (!isValidPassword)
+        throw new HttpError("Invalid credentials, could not log you in.", 401);
+    const token = jwt.sign(
+        {
+            id: existingDoctor.id,
+            email: existingDoctor.email,
+            type: "doctor",
+            name: existingDoctor.name,
+        },
+        process.env.JWT_KEY,
+        {
+            expiresIn: "6d",
+        }
+    );
+    const user = _.pick(existingDoctor, ["id", "name", "email"]);
+    res.status(201).json({ ...user, token });
 });
 
-exports.getProfile = async (req, res, next) => {
-  const doctorId = req.params.id;
-  let doctor;
-  try {
-    doctor = await Doctor.findById(doctorId);
-  } catch (error) {
-    return next(
-      new HttpError("Something went wrong, could not get doctor.", 500)
-    );
-  }
-  if (!doctor) {
-    return next(new HttpError("Could not find doctor.", 404));
-  }
-  res.status(200).json({ doctor: doctor.toObject({ getters: true }) });
-};
+exports.getProfile = catchAsync(async (req, res, next) => {
+    const doctorId = req.params.id;
+    const doctor = await Doctor.findById(doctorId);
+    if (!doctor) throw (new HttpError("Could not find doctor.", 404));
+    res.status(200).json({ doctor: doctor.toObject({ getters: true }) });
+});
 
-exports.editInfo = async (req, res, next) => {
-  const doctorId = req.params.doctorId;
-  const { phoneNo, password, specializations, qualifications, workplaces } =
-    req.body;
-
-  let updatedDoctor;
-  try {
-    updatedDoctor = await Doctor.findById(doctorId);
-  } catch (error) {
-    return next(
-      new HttpError("Something went wrong, could not update information.", 500)
-    );
-  }
-  if (!updatedDoctor) {
-    return next(
-      new HttpError("Something went wrong, could not update information.", 500)
-    );
-  }
-  let isValidPassword = false;
-  try {
+exports.editInfo = catchAsync(async (req, res, next) => {
+    const doctorId = req.params.doctorId;
+    const { phoneNo, password, specializations, qualifications, workplaces } = req.body;
+    const updatedDoctor = await Doctor.findById(doctorId);
+    if (!updatedDoctor) throw (new HttpError("Something went wrong, could not update information.", 500));
+    let isValidPassword = false;
     isValidPassword = await bcrypt.compare(password, updatedDoctor.password);
-  } catch (error) {
-    return next(
-      new HttpError("Something went wrong, could not update information.", 500)
-    );
-  }
-  if (!isValidPassword) {
-    return next(new HttpError("Invalid password, could not update info.", 401));
-  }
-  updatedDoctor.phoneNo = phoneNo;
-  updatedDoctor.specializations = specializations;
-  updatedDoctor.qualifications = qualifications;
-  updatedDoctor.workplaces = workplaces;
-  try {
+    if (!isValidPassword) throw (new HttpError("Invalid password, could not update info.", 401));
+    updatedDoctor.phoneNo = phoneNo;
+    updatedDoctor.specializations = specializations;
+    updatedDoctor.qualifications = qualifications;
+    updatedDoctor.workplaces = workplaces;
     await updatedDoctor.save();
-  } catch (error) {
-    console.log(error);
-    return next(
-      new HttpError("Something went wrong, could not update information", 500)
-    );
-  }
-  res.status(200).json({ doctor: updatedDoctor.toObject({ getters: true }) });
-};
+    res.status(200).json({ doctor: updatedDoctor.toObject({ getters: true }) });
+});
 
-exports.changePassword = async (req, res, next) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return next(new HttpError("Invalid inputs", 422));
-  }
-  const doctorId = req.params.doctorId;
-  const { oldPassword, newPassword } = req.body;
-
-  let updatedDoctor;
-  try {
-    updatedDoctor = await Doctor.findById(doctorId);
-  } catch (error) {
-    return next(
-      new HttpError("Something went wrong, could not update password.", 500)
-    );
-  }
-
-  if (!updatedDoctor) {
-    return next(
-      new HttpError("Something went wrong, could not update information.", 500)
-    );
-  }
-  let isValidPassword = false;
-  try {
+exports.changePassword = catchAsync(async (req, res, next) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) throw (new HttpError("Invalid inputs", 422));
+    const doctorId = req.params.doctorId;
+    const { oldPassword, newPassword } = req.body;
+    const updatedDoctor = await Doctor.findById(doctorId);
+    if (!updatedDoctor) throw (new HttpError("Something went wrong, could not update information.", 500));
+    let isValidPassword = false;
     isValidPassword = await bcrypt.compare(oldPassword, updatedDoctor.password);
-  } catch (error) {
-    return next(
-      new HttpError("Something went wrong, could not update password.", 500)
-    );
-  }
-  if (!isValidPassword) {
-    return next(
-      new HttpError("Invalid password, could not update password.", 401)
-    );
-  }
-  if (newPassword === oldPassword) {
-    return next(
-      new HttpError(
-        "Old password and new password are same, could not update password.",
-        401
-      )
-    );
-  }
-
-  let hashedPassword;
-  try {
-    hashedPassword = await bcrypt.hash(newPassword, 12);
-  } catch (error) {
-    console.log(error.message);
-    return next(
-      new HttpError("Something went wrong, could not update password.", 500)
-    );
-  }
-
-  updatedDoctor.password = hashedPassword;
-  try {
+    if (!isValidPassword) throw (new HttpError("Invalid password, could not update password.", 401));
+    if (newPassword === oldPassword) throw (new HttpError("Old password and new password are same, could not update password.", 401));
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+    updatedDoctor.password = hashedPassword;
     await updatedDoctor.save();
-  } catch (error) {
-    console.log(error);
-    return next(
-      new HttpError("Something went wrong, could not update password.", 500)
-    );
-  }
-  res.status(200).json({ doctor: updatedDoctor.toObject({ getters: true }) });
-};
+    res.status(200).json({ doctor: updatedDoctor.toObject({ getters: true }) });
+});
 
-exports.getDoctors = async (req, res, next) => {
-  const { doctorName, speciality } = req.query;
-  let doctors;
-  try {
-    doctors = await Doctor.find();
-  } catch (error) {
-    return next(
-      new HttpError("Something went wrong, could not get doctors.", 500)
-    );
-  }
-  if (!doctors) {
-    return next(new HttpError("Could not find any doctors.", 404));
-  }
-  let filteredDoctors;
-  if (doctorName && speciality) {
-    filteredDoctors = doctors.filter(
-      (doctor) =>
-        doctor.name.toLowerCase().includes(doctorName.toLowerCase()) &&
-        doctor.specializations.some((s) =>
-          s.toLowerCase().includes(speciality.toLowerCase())
-        )
-    );
-  } else if (doctorName) {
-    filteredDoctors = doctors.filter((doctor) =>
-      doctor.name.toLowerCase().includes(doctorName.toLowerCase())
-    );
-  } else if (speciality) {
-    filteredDoctors = doctors.filter((doctor) =>
-      doctor.specializations.some((s) =>
-        s.toLowerCase().includes(speciality.toLowerCase())
-      )
-    );
-  } else {
-    filteredDoctors = doctors;
-  }
-  res.status(200).json({
-    doctors: filteredDoctors.map((doctor) =>
-      doctor.toObject({ getters: true })
-    ),
-  });
-};
+exports.getDoctors = catchAsync(async (req, res, next) => {
+    const { doctorName, speciality } = req.query;
+    const doctors = await Doctor.find();
+    if (!doctors) throw (new HttpError("Could not find any doctors.", 404));
+    let filteredDoctors;
+    if (doctorName && speciality) {
+        filteredDoctors = doctors.filter(
+            (doctor) =>
+                doctor.name.toLowerCase().includes(doctorName.toLowerCase()) &&
+                doctor.specializations.some((s) =>
+                    s.toLowerCase().includes(speciality.toLowerCase())
+                )
+        );
+    } else if (doctorName) {
+        filteredDoctors = doctors.filter((doctor) =>
+            doctor.name.toLowerCase().includes(doctorName.toLowerCase())
+        );
+    } else if (speciality) {
+        filteredDoctors = doctors.filter((doctor) =>
+            doctor.specializations.some((s) =>
+                s.toLowerCase().includes(speciality.toLowerCase())
+            )
+        );
+    } else {
+        filteredDoctors = doctors;
+    }
+    res.status(200).json({
+        doctors: filteredDoctors.map((doctor) =>
+            doctor.toObject({ getters: true })
+        ),
+    });
+});
 
-exports.getDoctor = async (req, res, next) => {
-  const doctorId = req.params.doctorId;
-  let doctor;
-  try {
-    doctor = await Doctor.findById(doctorId);
-  } catch (error) {
-    return next(
-      new HttpError("Something went wrong, could not get doctor.", 500)
-    );
-  }
-  if (!doctor) {
-    return next(new HttpError("Could not find doctor.", 404));
-  }
-  res.status(200).json({ doctor: doctor.toObject({ getters: true }) });
-};
+exports.getDoctor = catchAsync(async (req, res, next) => {
+    const doctorId = req.params.doctorId;
+    const doctor = await Doctor.findById(doctorId);
+    if (!doctor) throw (new HttpError("Could not find doctor.", 404));
+    res.status(200).json({ doctor: doctor.toObject({ getters: true }) });
+});
 
 exports.getAllAppointments = catchAsync(async (req, res, next) => {
-  console.log(req.userData.id);
-
-  const appointments = await Appointment.find({
-    doctorId: req.userData.id,
-  }).populate("patientId");
-
-  console.log(appointments);
-
-  res.status(200).json({
-    data: {
-      appointments: appointments.map((appointment) =>
-        _.omit(appointment.toObject({ getters: true }), ["patientId.password"])
-      ),
-    },
-  });
+    console.log(req.userData.id);
+    const appointments = await Appointment.find({
+        doctorId: req.userData.id,
+    }).populate("patientId");
+    res.status(200).json({
+        data: {
+            appointments: appointments.map((appointment) =>
+                _.omit(appointment.toObject({ getters: true }), ["patientId.password"])
+            ),
+        },
+    });
 });
